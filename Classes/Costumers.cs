d47b19d52b58bae8d52afae89fbe20c5562cbfc4
Aldefas777 <mkrvs6@gmail.com>
2022-02-22 16:47:20 +0300

    Добавьте файлы проекта.

---
+++ Classes/Costumers.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Project.Classes
+{
+    public class Costumers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string SecondName { get; set; }
+        public string DateRegistration { get; set; }
+        public string Aboniment { get; set; }
+    }
+}
---
+++ Interfaces/ICostumerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Project;
+using Project.Classes;
+
+namespace Project.Interfaces
+{
+    public interface ICostumerRepository
+    {
+        List<Costumers> GetUsers();
+        void AddUsers(int? id, string names, string surname, string SecondName, string Aboniment);
+        void UpdateUser(int? Id, string names, string surname, string SecondName, string Aboniment);
+        IEnumerable<Costumers> DeleteUser(int? id);
+    }
+}
